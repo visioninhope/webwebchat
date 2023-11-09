b6d49be56ccc433875f668bdc227291a67144083
kerem <keremtiryaki@gmail.com>
2023-11-08 23:56:08 -0500

    Update debounceTimer type in debounce function

--- apps/web/src/lib/utils/debounce.ts
+++ apps/web/src/lib/utils/debounce.ts
@@ -1,5 +1,5 @@
 export function debounce(func: (...args: unknown[]) => void, delay: number) {
-    let debounceTimer: number;
+    let debounceTimer: ReturnType<typeof setTimeout>;
     return (...args: unknown[]) => {
         clearTimeout(debounceTimer);
         debounceTimer = setTimeout(() => func(...args), delay);
